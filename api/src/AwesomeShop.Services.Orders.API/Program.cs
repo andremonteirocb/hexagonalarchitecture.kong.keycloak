916fb60f4a11f57843d9d6d98a86a3662172f393
André Luiz Soares Alves Monteiro <andremonteiro.cb@hotmail.com>
2022-07-03 21:53:23 -0300

    ajustes de versões da api

--- api/src/AwesomeShop.Services.Orders.API/Program.cs
+++ api/src/AwesomeShop.Services.Orders.API/Program.cs
@@ -10,15 +10,11 @@ builder.Services
     .AddInfrastructure()
     .AddApplication();
 
-    
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,9 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
